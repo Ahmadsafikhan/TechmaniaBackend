d752d99e9e8b1e893836ad2685b68acfc3fe15a7
Ahmadsafikhan <safionair@gmail.com>
2023-09-18 16:08:01 +0500

    multer file uploading

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,6 @@
+import path from "path";
 import express from "express";
+
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
@@ -7,6 +9,7 @@ import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from "./routes/uploadRoutes.js"
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -31,6 +34,11 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 
 app.use(notFound);
 app.use(errorHandler);
