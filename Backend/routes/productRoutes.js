008a006541cdbcc8a43e47353f2f12b1f157a56a
Ahmadsafikhan <safionair@gmail.com>
2023-09-18 17:27:06 +0500

    product delete function

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
   getProductById,
   createProduct,
   updateProduct,
+  deleteProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
@@ -16,6 +17,6 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 
 // get specific product
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
 
 export default router;
